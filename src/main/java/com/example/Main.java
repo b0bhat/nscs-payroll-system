37691c6206f3e9effb1837811a543459f08ba01f
b0bhat <b0bhat.b0b@gmail.com>
2021-12-28 14:36:21 -0800

    client add

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -29,6 +29,7 @@ import org.w3c.dom.ranges.Range;
 import org.springframework.http.MediaType;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -78,7 +79,7 @@ public class Main {
     String employeeName = user.getName();
     String password = user.getPassword();
 
-    if (employeeName.equals("admin") && password.equals("123")){
+    if (employeeName.equals("admin") && password.equals("adminpassword")){
       flag = true;
       edit = true;
       return "redirect:/employees"; //CHANGE TO MAINPAGE FOR EACH LOGIN TYPE
@@ -95,10 +96,10 @@ public class Main {
         if (employeeName.equals(compareName) && password.equals(comparePassword)) {
           System.out.println("user exists");
           flag = true;
-          edit = true;
+          edit = false;
         } return "redirect:/employees"; //MAINPAGE
       }
-      return "error";
+      return "nouser";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
@@ -127,7 +128,7 @@ public class Main {
       if (flag && edit) {
         return "employees";
       } else {
-        return "error";
+        return "nouser";
       }
     } catch (Exception e) {
       model.put("message", e.getMessage());
@@ -162,28 +163,71 @@ public class Main {
     }
   }
 
-
-/*
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
+  @GetMapping("/employees/deleted")
+  public String deleteEmployee(Map<String, Object> model, @RequestParam String e_id) {
     try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      String sql = "DELETE FROM login WHERE employeeName =?";
+      PreparedStatement ps = connection.prepareStatement(sql);
+      ps.setString(1, e_id);
+      ps.executeUpdate();
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
+      return "redirect:/employees";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "error";
     }
-  }*/
+  }
+
+  @GetMapping("/clients")
+  String clientList(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+
+      String sql = "SELECT * FROM clients";
+      ResultSet rs = stmt.executeQuery(sql);
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add(rs.getString("clientName"));
+      }
+      model.put("clients", output);
+      if (flag && edit) {
+        return "clients";
+      } else {
+        return "nouser";
+      }
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
+
+  @GetMapping("/clients/add")
+  public String returnClientAdd(Map<String, Object> model) throws Exception {
+    String client = new String();
+    model.put("client", client);
+    if (flag && edit) {
+      return "employees/addClient";
+    } else {
+      return "error";
+    }
+  }
+
+  @PostMapping(path = "/clients/add", consumes = { MediaType.APPLICATION_FORM_URLENCODED_VALUE })
+  public String handleClientAdd(Map<String, Object> model, String client) throws Exception {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+
+      String sql = "INSERT INTO clients VALUES ('"
+          + client + "')";
+
+      stmt.executeUpdate(sql);
+      return "redirect:/clients"; // Directly returns to employee homepage
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
+  }
 
   @Bean
   public DataSource dataSource() throws SQLException {
