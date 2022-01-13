53a8b4c175437c78f41638cd9db637e50d2a6c44
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 12:11:06 -0800

    fff

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.example;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -35,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .withUser("admin").password(passwordEncoder().encode("123")).roles("ADMIN");
   }
   
+  @Autowired
   SecurityHandler securityHandler;
 
   @Override
