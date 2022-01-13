f0ff6f819c0ea4e750d6dbd379c59a0fb5d51749
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 13:18:22 -0800

    fff

--- src/main/java/com/example/SecurityHandler.java
+++ src/main/java/com/example/SecurityHandler.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 @Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class SecurityHandler implements AuthenticationSuccessHandler {
 
      public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException  {
