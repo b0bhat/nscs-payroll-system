9d710832c7caed33527862bcb67382b91d60ae1b
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 12:00:50 -0800

    fff

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -71,7 +71,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 }
             }
         })
-	    .defaultSuccessUrl("/admin/clients", false)
+	    //.defaultSuccessUrl("/admin/clients", false)
 	    .failureUrl("/nouser.html")
   	.and()
   	.exceptionHandling().accessDeniedPage("/nouser");
