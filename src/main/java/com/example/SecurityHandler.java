ace3f189c0e455bf685769b97d45e345cb9d6082
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 12:15:31 -0800

    fff

--- src/main/java/com/example/SecurityHandler.java
+++ src/main/java/com/example/SecurityHandler.java
@@ -16,9 +16,9 @@ public class SecurityHandler implements AuthenticationSuccessHandler {
      public void onAuthenticationSuccess(HttpServletRequest request,   HttpServletResponse response, Authentication authentication) throws IOException  {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
         if (roles.contains("ADMIN")) {
-            response.sendRedirect("/admin/clients.html");
+            response.sendRedirect("/admin/clients");
         } else if (roles.contains("USER")) {
-            response.sendRedirect("/user/home.html");
-        } else response.sendRedirect("/nouser.html");
+            response.sendRedirect("/user/home");
+        } else response.sendRedirect("/nouser");
     }
 }
