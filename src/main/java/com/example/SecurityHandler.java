4618079dc350b8b07f7f1e4727057f38299e87b6
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 12:23:09 -0800

    fff

--- src/main/java/com/example/SecurityHandler.java
+++ src/main/java/com/example/SecurityHandler.java
@@ -7,17 +7,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityHandler implements AuthenticationSuccessHandler {
 
-     public void onAuthenticationSuccess(HttpServletRequest request,   HttpServletResponse response, Authentication authentication) throws IOException  {
-        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ADMIN")) {
+     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException  {
+    	 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+         String role = auth.getAuthorities().toString();
+        if (role.contains("ADMIN")) {
             response.sendRedirect("/admin/clients");
-        } else if (roles.contains("USER")) {
+        } else if (role.contains("USER")) {
             response.sendRedirect("/user/home");
         } else response.sendRedirect("/nouser");
     }
