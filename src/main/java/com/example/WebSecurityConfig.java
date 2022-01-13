f2f8d88a6917c6db72f6b9f78d6045967dd657c6
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 13:27:27 -0800

    fff

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,8 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
        return new AppAuthenticationSuccessHandler();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+  @Autowired
+  protected void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
   auth.inMemoryAuthentication()
       .withUser("user1").password(passwordEncoder().encode("123")).roles("USER")
       .and()
@@ -41,6 +43,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+	  CharacterEncodingFilter filter = new CharacterEncodingFilter();
+      filter.setEncoding("UTF-8");
+      filter.setForceEncoding(true);
+      http.addFilterBefore(filter,CsrfFilter.class);
   http
     .csrf().disable()
     .authorizeRequests()
