185c406a397e80c7883dc9ddb47171d1564f4d30
b0bhat <b0bhat.b0b@gmail.com>
2022-01-13 18:21:02 -0800

    test

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	    .anyRequest().authenticated()
 	    .and()
     .formLogin()
-	    .loginPage("/login", "/*")
+	    .loginPage("/login")
 	    .loginProcessingUrl("/login")
 	    .usernameParameter("username")
 	    .passwordParameter("password")
