619d4c27fc560aff4d69c7c6039937a821112b1f
b0bhat <b0bhat.b0b@gmail.com>
2022-01-14 12:20:46 -0800

    test

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
 	  auth.jdbcAuthentication()
       .dataSource(dataSource)
-      .usersByUsernameQuery("SELECT \"employeeName\", password, true" + " from login where \"employeeName\"=?")
+      .usersByUsernameQuery("SELECT \"employeeName\" AS username, CONCAT('{noop}',password), true AS enabled" + " from login where \"employeeName\"=?")
       .authoritiesByUsernameQuery("SELECT \"employeeName\", 'USER' AS authority FROM login where \"employeeName\"=?");
 	  /*
 	  auth.inMemoryAuthentication()
